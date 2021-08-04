a447e060c7388036f399b659ad1c9ea735646ef5
programador1 <Kenvi.009@gmail.com>
2021-08-04 16:53:20 -0500

    Coins Stack

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import CoinsStack from './src/components/coins/CoinsStack';
 
 const App = () => {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <CoinsStack></CoinsStack>
+    </NavigationContainer>
+  );
 };
 
 export default App;
