45af028103200c1d656458e6f68062aa33ebd5bb
programador1 <Kenvi.009@gmail.com>
2021-08-05 10:37:57 -0500

    Flatlist of coins

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,25 +1,31 @@
 import React, {Component} from 'react';
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {View, Text, Pressable, StyleSheet, FlatList} from 'react-native';
 import Http from 'tripmates_frontend/src/libs/http';
 class CoinsScreen extends Component {
+  state = {
+    coins: [],
+  };
+
   componentDidMount = async () => {
     const coins = await Http.instance.get(
       'https://api.coinlore.net/api/tickers/',
     );
-    console.log('coins: ', coins);
+    this.setState({coins: coins.data});
   };
 
   handlePress = () => {
     console.log('go to hola', this.props);
     this.props.navigation.navigate('CoinsDetails');
   };
+
   render() {
+    const {coins} = this.state;
     return (
       <View style={styles.container}>
-        <Text>Coins Screen</Text>
-        <Pressable style={styles.btn} onPress={this.handlePress}>
-          <Text style={styles.btnText}>ir a details</Text>
-        </Pressable>
+        <FlatList
+          data={coins}
+          renderItem={({item}) => <Text>{item.name}</Text>}
+        />
       </View>
     );
   }
