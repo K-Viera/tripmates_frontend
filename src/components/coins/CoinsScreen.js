7c8cc586d5c23578050eb4f44588360bd2494262
programador1 <Kenvi.009@gmail.com>
2021-08-05 08:51:54 -0500

    Send fetch REST API

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text, Pressable, StyleSheet} from 'react-native';
-
+import Http from 'tripmates_frontend/src/libs/http';
 class CoinsScreen extends Component {
+  componentDidMount = async () => {
+    const coins = await Http.instance.get(
+      'https://api.coinlore.net/api/tickers/',
+    );
+    console.log('coins: ', coins);
+  };
+
   handlePress = () => {
     console.log('go to hola', this.props);
     this.props.navigation.navigate('CoinsDetails');
