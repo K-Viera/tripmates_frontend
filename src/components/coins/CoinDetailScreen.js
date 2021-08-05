6e19175c8fc84f2f91d66b5cc3d70145e64aeb39
programador1 <Kenvi.009@gmail.com>
2021-08-05 08:02:44 -0500

    Navigation in CoinsScreens

---
+++ src/components/coins/CoinDetailScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class CoinDetailScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Coin Details Screen</Text>
+      </View>
+    );
+  }
+}
+export default CoinDetailScreen;
--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
 class CoinsScreen extends Component {
+  handlePress = () => {
+    console.log('go to hola', this.props);
+    this.props.navigation.navigate('CoinsDetails');
+  };
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Coins Screen</Text>
+        <Pressable style={styles.btn} onPress={this.handlePress}>
+          <Text style={styles.btnText}>ir a details</Text>
+        </Pressable>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'red'},
+  btn: {padding: 8, backgroundColor: 'blue', borderRadius: 8, margin: 16},
+  btnText: {color: '#fff', textAlign: 'center'},
+});
 export default CoinsScreen;
