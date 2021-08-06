371063d8421a6664d1605c2769933dc86864b67e
programador1 <Kenvi.009@gmail.com>
2021-08-06 14:57:25 -0500

    coin details screen

--- src/components/coins/CoinDetailScreen.js
+++ src/components/coins/CoinDetailScreen.js
@@ -2,13 +2,21 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 class CoinDetailScreen extends Component {
+  state = {
+    coin: {},
+  };
   componentDidMount() {
-    console.log('coin', this.props.route.params);
+    const {coin} = this.props.route.params;
+    this.props.navigation.setOptions({title: coin.symbol});
+    this.setState({coin});
   }
   render() {
+    const {coin} = this.state;
     return (
       <View>
-        <Text>Coin Details Screen</Text>
+        <View>
+          <Text>{coin.name}</Text>
+        </View>
       </View>
     );
   }
