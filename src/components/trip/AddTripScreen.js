48880818add52ef2b491c7a7a4cebf58073a59d0
Kevin <kenvi.009@gmail.com>
2021-09-20 10:34:02 -0500

    Set finishDate an beginDate

--- src/components/trip/AddTripScreen.js
+++ src/components/trip/AddTripScreen.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, TextInput, Button, Alert} from 'react-native';
+import {View, StyleSheet, TextInput, Button, Alert, Text} from 'react-native';
 import Colors from '../../res/colors';
 import axios from 'axios';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from 'moment';
 
 class AddTripScreen extends Component {
   state = {
     from: '',
     to: '',
+    Interests: '',
+    dateEnum: '',
+    show: false,
     beginDate: '',
     finishDate: '',
-    Interests: '',
-    date: new Date(Date.now()),
-    show: false,
-    dateFinish: new Date(Date.now()),
   };
 
   addTrip = async () => {
@@ -37,14 +37,30 @@ class AddTripScreen extends Component {
   onChangeDatePicker = async (event, selectedDate) => {
     const currentDate = selectedDate || date;
     const showValue = Platform.OS === 'ios';
+    if (this.state.dateEnum == 1) {
+      await this.setState({
+        beginDate: currentDate,
+      });
+      console.log(this.state.beginDate);
+    } else if (this.state.dateEnum == 2) {
+      await this.setState({
+        finishDate: currentDate,
+      });
+    }
     await this.setState({
-      date: currentDate,
       show: showValue,
     });
   };
-  showDatepicker = async () => {
+  showDatepickerInit = async () => {
     await this.setState({
       show: true,
+      dateEnum: 1,
+    });
+  };
+  showDatepickerFinish = async () => {
+    await this.setState({
+      show: true,
+      dateEnum: 2,
     });
   };
 
@@ -63,24 +79,19 @@ class AddTripScreen extends Component {
           placeholder="to"
           style={styles.inputText}
         />
-        <Button onPress={this.showDatepicker} title="Show date picker!" />
-        {this.state.show && (
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={this.state.date}
-            mode={'date'}
-            is24Hour={true}
-            display="default"
-            onChange={this.onChangeDatePicker}
-          />
-        )}
-        {/* <TextInput
-          onChangeText={text => this.setState({beginDate: text})}
-          value={this.state.beginDate}
-          placeholder="Nombre"
-          style={styles.inputText}
-        /> */}
-        <Button onPress={this.showDatepicker} title="Show date picker!" />
+        <Text>
+          {this.state.beginDate != ''
+            ? moment(this.state.beginDate).format('YYYY-MM-DD')
+            : ''}{' '}
+        </Text>
+        <Button onPress={this.showDatepickerInit} title="Show date picker!" />
+
+        <Text>
+          {this.state.finishDate != ''
+            ? moment(this.state.finishDate).format('YYYY-MM-DD')
+            : ''}{' '}
+        </Text>
+        <Button onPress={this.showDatepickerFinish} title="Show date picker!" />
         {/* <TextInput
           onChangeText={text => this.setState({finishDate: text})}
           value={this.state.finishDate}
@@ -99,6 +110,16 @@ class AddTripScreen extends Component {
           onPress={this.addTrip}
           style={styles.btn}
         />
+        {this.state.show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={new Date(Date.now())}
+            mode={'date'}
+            is24Hour={true}
+            display="default"
+            onChange={this.onChangeDatePicker}
+          />
+        )}
       </View>
     );
   }
