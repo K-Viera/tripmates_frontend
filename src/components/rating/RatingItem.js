7ce0ec319edc87d52a5088c5108f7d473a3dff9b
juan rios <juan.rios49@eia.edu.co>
2021-10-01 20:05:50 -0500

    sprint 2 ok

--- src/components/rating/RatingItem.js
+++ src/components/rating/RatingItem.js
@@ -2,32 +2,133 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import Colors from '../../res/colors';
 import moment from 'moment';
+import storage from '../../libs/storage';
+import axios from 'axios';
+import {Swipeable} from 'react-native-gesture-handler';
 
 class RatingItem extends Component {
   constructor(props) {
     super(props);
+    this.leftAction = this.leftAction.bind(this);
+    this.renderLeftAction = this.renderLeftAction.bind(this);
+    this.rightAction = this.rightAction.bind(this);
+    this.renderRightAction = this.renderRightAction.bind(this);
   }
+
+  renderLeftAction = () => {
+    const {item} = this.props;
+    return (
+      <View style={styles.leftAction}>
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <Text style={styles.symbolText}>{item.user.name}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.nameText}>{item.comment}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.symbolText}>{item.rating}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.nameText}>
+              {moment(item.date).format('MMMM DD YYYY')}
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  leftAction = async rate => {
+    const url = 'https://still-shore-58656.herokuapp.com/api/rate/approve';
+    const token = await storage.instance.get('access-token');
+
+    const config = {
+      method: 'put',
+      url: url,
+      headers: {
+        'access-token': token,
+      },
+      data: {
+        rating: rate._id,
+        aprove: true,
+      },
+    };
+
+    const res = await axios(config);
+    console.log(res.data.data);
+  };
+
+  renderRightAction = () => {
+    const {item} = this.props;
+    return (
+      <View style={styles.rightAction}>
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <Text style={styles.symbolText}>{item.user.name}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.nameText}>{item.comment}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.symbolText}>{item.rating}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.nameText}>
+              {moment(item.date).format('MMMM DD YYYY')}
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  rightAction = async rate => {
+    const url = 'https://still-shore-58656.herokuapp.com/api/rate/approve';
+    const token = await storage.instance.get('access-token');
+
+    const config = {
+      method: 'put',
+      url: url,
+      headers: {
+        'access-token': token,
+      },
+      data: {
+        rating: rate._id,
+        aprove: false,
+      },
+    };
+
+    const res = await axios(config);
+    console.log(res.data.data);
+  };
 
   render() {
     const {item} = this.props;
 
     return (
-      <View style={styles.container}>
-        <View style={styles.row}>
-          <Text style={styles.symbolText}>{item.user.name}</Text>
+      <Swipeable
+        renderLeftActions={this.renderLeftAction}
+        onSwipeableLeftOpen={() => this.leftAction(item)}
+        renderRightActions={this.renderRightAction}
+        onSwipeableRightOpen={() => this.rightAction(item)}>
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <Text style={styles.symbolText}>{item.user.name}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.nameText}>{item.comment}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.symbolText}>{item.rating}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.nameText}>
+              {moment(item.date).format('MMMM DD YYYY')}
+            </Text>
+          </View>
         </View>
-        <View style={styles.row}>
-          <Text style={styles.nameText}>{item.comment}</Text>
-        </View>
-        <View style={styles.row}>
-          <Text style={styles.symbolText}>{item.rating}</Text>
-        </View>
-        <View style={styles.row}>
-          <Text style={styles.nameText}>
-            {moment(item.date).format('MMMM DD YYYY')}
-          </Text>
-        </View>
-      </View>
+      </Swipeable>
     );
   }
 }
@@ -55,6 +156,16 @@ const styles = StyleSheet.create({
     color: Colors.blackPearl,
     fontSize: 14,
     marginRight: 16,
+  },
+  leftAction: {
+    backgroundColor: Colors.green,
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  rightAction: {
+    backgroundColor: Colors.carmine,
+    justifyContent: 'space-between',
+    width: '100%',
   },
 });
 
--- src/components/trip/TripDetailScreen.js
+++ src/components/trip/TripDetailScreen.js
@@ -1,15 +1,16 @@
-import React, {Component, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import Colors from '../../res/colors';
 import storage from '../../libs/storage';
 import axios from 'axios';
-import {useLogin} from '../../libs/LoginProvider';
 
 const TripDetailScreen = props => {
   const [trip, setTrip] = useState({});
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     getTrip();
+    getProfile();
   }, []);
 
   const getTrip = async () => {
@@ -31,12 +32,52 @@ const TripDetailScreen = props => {
     setTrip(res.data.data);
   };
 
+  const deleteTrip = async () => {
+    const url = 'https://still-shore-58656.herokuapp.com/api/trip/';
+    const token = await storage.instance.get('access-token');
+
+    const config = {
+      method: 'delete',
+      url: url,
+      headers: {
+        'access-token': token,
+        trip: trip._id,
+      },
+    };
+
+    const res = await axios(config);
+    console.log(res.data.data);
+
+    props.navigation.navigate('Mis Viajes');
+  };
+
+  const getProfile = async () => {
+    const url = 'https://still-shore-58656.herokuapp.com/api/user/mine';
+    const token = await storage.instance.get('access-token');
+
+    const config = {
+      method: 'get',
+      url: url,
+      headers: {
+        'access-token': token,
+      },
+    };
+    const res = await axios(config);
+    console.log(res.data.data);
+    setUser(res.data.data);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{trip.from}</Text>
       <Text style={styles.linkText}>{trip.to}</Text>
       <Text style={styles.text}>{trip.beginDate}</Text>
       <Text style={styles.linkText}>{trip.finishDate}</Text>
+      {user._id === trip.user && (
+        <Text style={styles.btnText} onPress={() => deleteTrip()}>
+          Eliminar Viaje
+        </Text>
+      )}
     </View>
   );
 };
@@ -57,8 +98,14 @@ const styles = StyleSheet.create({
     margin: 16,
   },
   btnText: {
-    color: Colors.blackPearl,
+    color: Colors.white,
     textAlign: 'center',
+    fontWeight: 'bold',
+    backgroundColor: Colors.zircon,
+    borderRadius: 15,
+    margin: 25,
+    marginBottom: -5,
+    padding: 15,
   },
   loader: {
     marginTop: 60,
